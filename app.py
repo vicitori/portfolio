2441f2a662ce3c199f82304f11ad1e4e6d091dbf
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2024-12-23 05:22:37 +0300

    feat: add bootstrap template
    Signed-off-by: Lutsyuk Victoria <victorialutsyuk.t@gmail.com>

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, url_for, redirect # to redirect from page to page.
+app = Flask(__name__) # instance of a flask web application.
+
+@app.route('/') # should write path where we can get this function.
+def index(): # function that returns home-page
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
